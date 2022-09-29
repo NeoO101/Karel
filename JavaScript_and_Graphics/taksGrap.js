d3a528eed1738e97b5d280b0e4a135dc201cf720
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-09-29 23:35:53 +0400

    this is graphics example tasks file

---
+++ JavaScript_and_Graphics/taksGrap.js
@@ -0,0 +1,8 @@
+//This program will add text that 
+//says "Hello world" at 100 , 100
+
+function start(){
+    var text = new Text("Hello world");
+    text.setPosition(100, 100);
+    add(text);
+}

caa157774e7f8596a6149c2643d7039370f9e246
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-09-30 00:14:23 +0400

    I finish graphics

--- JavaScript_and_Graphics/taksGrap.js
+++ JavaScript_and_Graphics/taksGrap.js
@@ -5,4 +5,16 @@ function start(){
     var text = new Text("Hello world");
     text.setPosition(100, 100);
     add(text);
+
+
+    var circle = new Circle(30);
+    circle.setPosition(100, 50);
+    circle.setColor(Color.blue);
+    add(circle);
+
+    var rect = new Rctangle(100, 50);
+    rect.setPosition(60, 150);
+    rect.setColor(Color.red);
+    add(rect);
+    
 }

39e6a9bce028bacedf12ce362b793ba309c96c55
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-09-29 16:21:44 +0400

    this is javascritps task in 3 lesen

---
+++ JavaScript_and_Graphics/tasks3_JS.js
@@ -0,0 +1,16 @@
+/*
+Write a program that prints out your name on one line, and then on the next line says a hobby you have.
+
+Your output must be in this format:
+*/
+
+/* This program prints out "Hello, world" */
+
+function start(){
+    println("Hello world");
+    /* This function should print out your 
+    * name, and a hobby you have */
+    println("My name is Rezo");
+    println("I like to Basketball and reading");
+
+}

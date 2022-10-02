736e07da1184ef0880344fc9959d61538146d8b5
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-03 01:58:54 +0400

    randomizer and while loop, also i need break

--- JavaScript_and_Graphics/whileBreak.js
+++ JavaScript_and_Graphics/whileBreak.js
@@ -16,5 +16,22 @@ function start(){
     println("The total was: " + sum);
 
 
-
+    function start(){
+        var count = 0;
+    
+        while(true){
+            //var count = 0;
+            
+            var rollOne = Randomizer.nextInt(1, 6);
+            var rollTwo = Randomizer.nextInt(1, 6);
+            
+            println("rolled: " + rollOne + " " + rollTwo);
+            count += 1;
+            if(rollOne == 1 && rollTwo == 1){
+                println("It took you " + count +" rolls to get snake eyes");
+                break;
+            }
+        }
+    
+    }
 }

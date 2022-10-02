52b950fb400700bafb5f1b49891764dfd6efbaea
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-03 01:31:43 +0400

    black and red circles

--- JavaScript_and_Graphics/fibonacci.js
+++ JavaScript_and_Graphics/fibonacci.js
@@ -13,4 +13,28 @@ function start(){
         i = i + b;
         b = c;
     }
+
+
+    var circle;
+    var change = 20;
+    var count = 0;
+
+    var radous = 200;
+
+    var pos_X = getWidth() / 2;
+    var pos_Y = getHeight() / 2;
+
+    while(radius >= 0){
+        var circle = new Circle(radius);
+        circle.setPosition(pos_X, pos_Y);
+        if( count % 2 == 0){
+            circle.setColor(Color.black);
+        }else{
+            circle.setColor(Color.red);
+        }
+        add(circle);
+        count++;
+        radius = radius - change;
+    }
+
 }

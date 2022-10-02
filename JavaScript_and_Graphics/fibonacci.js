5b10d3b4761862bfc4272ddf18bf9fbb1c2ee5e1
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-03 00:59:35 +0400

    all done, fibonacci

---
+++ JavaScript_and_Graphics/fibonacci.js
@@ -0,0 +1,16 @@
+var Max = 1000;
+
+function start(){
+
+    var i = 1;
+    var b = 0;
+
+    var c = 0;
+
+    while( MAX > i){
+        println(i);
+        c = i;
+        i = i + b;
+        b = c;
+    }
+}

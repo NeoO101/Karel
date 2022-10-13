4f5ee93682eb012e4fa2129fcee8cd203d6d8378
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-13 13:37:09 +0400

    This is square funtion

---
+++ JavaScript_and_Graphics/function.js
@@ -0,0 +1,36 @@
+//This program doubles several
+
+
+function start(){
+
+doubleNumber(5);
+
+doubleNumber(12);
+
+var y = 3;
+
+doubleNubmer(y);
+
+    square(5);
+	square(10);
+	
+	var number = 12;
+	
+	square(number);
+
+}
+
+//This function doubles a giv number
+//parameters
+
+function doubleNubmer (x){
+    var doubleX = 2 * x;
+    println(doubleX);
+}
+
+//funtion for number square
+function square(x){
+    
+    var squareNumber = x * x;
+    println(squareNumber);
+}

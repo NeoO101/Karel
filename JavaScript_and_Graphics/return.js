b6ed3ccd068b5f8a1380b15f6fa9fe45ee04bdd4
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-13 18:00:42 +0400

    Gymnastics Mats

--- JavaScript_and_Graphics/return.js
+++ JavaScript_and_Graphics/return.js
@@ -26,3 +26,32 @@ function square(x){
     var result = x * x;
     return result;
 }
+function start() {
+    var width = readLine("What is the width of the mat (in feet)? ");
+    
+    var length = readLine("What is the length of the mat (in feet)? ");
+    
+    var square = calculateArea(width, length);
+    println("The area of each mat is " + square + " square feet.");
+    
+    
+    var roomWidth = readLine("What is the width of the room (in feet)? ");
+    
+    var roomLength = readLine("What is the length of the mat (in feet)? ");
+    
+    var squareRoom = calculateArea(roomWidth, roomLength);
+    
+    println("The area of the room is" + squareRoom +"square feet. ");
+    
+    var mats = squareRoom / square;
+    
+    println("You'll need" + mats + "mats to cover the floor.");
+}
+
+
+// This function should calculate the area of a rectangle using
+// the parameters of width and length.
+function calculateArea(width, length){
+    var result = width * length;
+    return result;
+}

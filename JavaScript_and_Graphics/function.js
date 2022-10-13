69e2a6af60e781ab452998a19b3b9313a232fecc
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-13 16:03:17 +0400

    Pool table

--- JavaScript_and_Graphics/function.js
+++ JavaScript_and_Graphics/function.js
@@ -136,3 +136,28 @@ function verticalLine(first, second){
     add(line);
     
 }
+
+var POOL_BALL_RADIUS = 40;
+var POOL_BALL_FONT = "30pt Arial";
+
+function start(){
+	drawPoolBall(Color.orange, 5, 100, 100);
+	drawPoolBall(Color.green, 6, 50, 200);
+	drawPoolBall(Color.red, 3, 150, 350);
+	drawPoolBall(Color.blue, 2, 250, 140);
+
+	// Add some more pool balls!
+}
+
+
+function drawPoolBall(color, num, x, y){
+	var circle = new Circle(POOL_BALL_RADIUS);
+	circle.setColor(color);
+	circle.setPosition(x, y);
+	add(circle);
+	
+	var text = new Text(num, POOL_BALL_FONT);
+	text.setColor(Color.white);
+	text.setPosition(x, y);
+	add(text);
+	

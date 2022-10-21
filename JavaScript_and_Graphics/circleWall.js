23ae7f7f21464165724f7b44060b1ad057b393cd
Revaz <revaz.kapianidze06@geolab.edu.ge>
2022-10-22 00:00:14 +0400

    random rectangle

--- JavaScript_and_Graphics/circleWall.js
+++ JavaScript_and_Graphics/circleWall.js
@@ -44,3 +44,70 @@ function drawCircle(){
     }
 
 }
+
+// Constants
+
+/*
+var WIDTH = 50;
+var HEIGHT = 20;
+
+
+function start() {
+    
+}
+
+*/
+
+// Constants
+
+var WIDTH = 50;
+
+var HEIGHT = 20;
+
+var Y= getHeight()-HEIGHT;
+
+var X=0;
+
+var counter=0;
+
+
+
+function start() {
+
+setTimer(red, 50);
+
+}
+
+
+
+function red() {
+
+var red = new Rectangle(WIDTH,HEIGHT);
+
+red.setPosition(X, Y);
+
+red.setColor(Color.randomRed());
+
+//counter++;
+
+add(red);
+
+X+=WIDTH;
+
+
+
+if (X>getWidth()) {
+
+X=(0);
+
+Y-=HEIGHT;
+
+}
+
+if(Y<0) {
+
+stopTimer(red);
+
+}
+
+}
